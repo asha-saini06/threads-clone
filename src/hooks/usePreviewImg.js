6b6ee5cef0defd65a9aae0a4a59513dcf180feb7
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-05-15 20:28:22 +0530

    Update User Profile functionality

---
+++ src/hooks/usePreviewImg.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import useShowToast from "./useShowToast";
+
+
+const usePreviewImg = () => {
+    const [imgUrl, setImgUrl] = useState(null);
+    const showToast = useShowToast();
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0]; // Get the selected file from the input element
+
+        // console.log(file);
+        if (file && file.type.startsWith("image/")) { // Check if the file is an image
+            const reader = new FileReader(); // Create a new instance of FileReader
+
+            reader.onloadend = () => {
+                setImgUrl(reader.result); // Set the image URL for preview
+            }; // Define a callback function for when file reading is completed
+
+            reader.readAsDataURL(file);  // Read the file as a base64 data URL
+        } else {
+            showToast("Error", "Please select an image file", "error");
+            setImgUrl(null); // Set the image URL to null if the file is not an image
+        } // Display an error message if the file is not an image
+    };
+
+    return { handleImageChange, imgUrl }
+}
+
+export default usePreviewImg
