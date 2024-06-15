27b05f19d9a4ce3f02e12663fe77f9973b49b8be
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-06-15 23:54:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
+import job from "./cron/cron.js";
 
 dotenv.config(); // To load the environment variables
 
